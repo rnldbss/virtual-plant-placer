563aa7d3890ed8c4ba4b6fdd63920ff9938faa7b
rnldbss <ronaldbessam@gmail.com>
2025-04-15 09:17:25 +0100

    refactor: small layout changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,8 +8,8 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="bg-level-0 h-screen w-screen flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-3xl text-secondary">Virtual Plant Placer</h1>
+      <div className="text-center space-y-8">
+        <h1 className="text-4xl text-secondary">Virtual Plant Placer</h1>
         <Link
           className="bg-accent px-3 py-1 text-level-0 rounded-md"
           to={"/garden"}
